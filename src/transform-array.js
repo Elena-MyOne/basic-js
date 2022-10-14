a754ed9c208496bc43b5ec7cd340e8b6fae35ff5
Elena <myone72@yahoo.com>
2022-10-13 23:55:24 -0400

    feat: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  if(arr.includes(!typeof number)) return arr;
+  if(!arr.includes('--discard-prev') && !arr.includes('--double-prev') && !arr.includes('--double-next') && !arr.includes('--discard-next')) return arr;
+
+  const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+      if(arr[i] === '--discard-prev') {
+        result.push(...arr)
+        if(i === 0) {
+          result.shift()
+        } else {
+          result.splice(i - 1, 2);
+        }
+      }
+    
+      if(arr[i] === '--discard-next') {
+        result.push(...arr)
+        if(i === arr.length -1) {
+          result.pop()
+        } else if(arr[i + 2] === '--double-prev' || arr[i + 2] === '--discard-prev') {
+          result.splice(i, 3);
+          return result
+        } else {
+          result.splice(i, 2);
+        }
+      }
+    
+      if(arr[i] === '--double-prev') {
+        result.push(...arr)
+        if(i === 0) {
+          result.shift()
+        } else {
+          result.splice(i, 1, arr[i - 1]);
+        }
+      }
+    
+      if(arr[i] === '--double-next') {
+        result.push(...arr)
+        if(i === arr.length -1) {
+          result.pop()
+        } else if(arr[i + 2] === '--double-prev') {
+          result.splice(i, 1, arr[i + 1], arr[i + 1]);
+          result.splice(i + 3, 1);
+          return result;
+        } else if(arr[i + 2] === '--discard-prev') {
+          result.splice(i, 1);
+          result.splice(i + 1, 1)
+          return result
+        } else {
+          result.splice(i, 1, arr[i + 1]);
+        }
+      }
+    }
+
+return result
 }
 
 module.exports = {
